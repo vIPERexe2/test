0a559f09628b0f69e7bf4d392abe8abaaf60e962
vIPERexe2 <148722243+vIPERexe2@users.noreply.github.com>
2023-10-28 14:01:02 +0300

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -1,6 +1,4 @@
-import requests
-
-proxies = [
+proxy_list = [
     "138.2.61.140:88",
     "123.182.58.221:8089",
     "185.189.199.75:23500",
@@ -13,24 +11,19 @@ proxies = [
     "170.81.241.14:999",
     "185.189.199.75:23500",
     "177.185.92.193:3128"
-    # Add more proxy servers as needed
 ]
 
-def write_callback(response, contents):
-    response += contents
-    return len(contents)
+def connect_to_proxy(proxy):
+    # Code to connect to the proxy
+    pass
 
-def make_network_request(url):
-    for proxy in proxies:
-        proxy_host, proxy_port = proxy.split(':')
-        proxies = {
-            'http': f'http://{proxy_host}:{proxy_port}',
-            'https': f'http://{proxy_host}:{proxy_port}'
-        }
+def make_proxy_multi_terminal(proxy_list):
+    for proxy in proxy_list:
         try:
-            response = requests.get(url, proxies=proxies)
-            if response.status_code == 200:
-                return response.text
-        except requests.exceptions.RequestException:
-            pass
-    raise Exception("Failed to make a successful request using any proxy server.")
+            connect_to_proxy(proxy)
+            print(f"Connected to proxy: {proxy}")
+        except Exception as e:
+            print(f"Failed to connect to proxy: {proxy}")
+            print(f"Error: {str(e)}")
+
+make_proxy_multi_terminal(proxy_list)
