9e77b1beb4ec6a15e6df5aef64868a8a79402513
vIPERexe2 <148722243+vIPERexe2@users.noreply.github.com>
2023-10-28 14:18:40 +0300

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -1,34 +1,48 @@
-import requests
+import socket
+import threading
 
-def connect_to_proxy(proxy):
-    proxies = {
-        'http': f'http://{proxy}',
-        'https': f'http://{proxy}'
-    }
-    try:
-        response = requests.get('https://www.example.com', proxies=proxies)
-        if response.status_code == 200:
-            print(f"Connected to proxy: {proxy}")
-        else:
-            print(f"Failed to connect to proxy: {proxy}")
-    except requests.exceptions.RequestException as e:
-        print(f"Failed to connect to proxy: {proxy}")
-        print(f"Error: {str(e)}")
+class ProxyServer:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.bind((self.host, self.port))
+        self.server_socket.listen(5)
+        print(f"Proxy server started on {self.host}:{self.port}")
 
-proxies = [
-    "138.2.61.140:88",
-    "123.182.58.221:8089",
-    "185.189.199.75:23500",
-    "164.132.170.100:80",
-    "153.92.214.224:80",
-    "120.196.207.10:80",
-    "31.40.27.214:3128",
-    "153.92.214.224:80",
-    "177.185.92.193:3128",
-    "170.81.241.14:999",
-    "185.189.199.75:23500",
-    "177.185.92.193:3128"
-]
+    def handle_client(self, client_socket):
+        request = client_socket.recv(4096)
+        print(f"Received request: {request.decode()}")
 
-for proxy in proxies:
-    connect_to_proxy(proxy)
+        # Modify the request if needed
+        # ...
+
+        # Forward the modified request to the destination server
+        destination_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        destination_socket.connect(("destination_server_host", destination_server_port))
+        destination_socket.sendall(request)
+
+        # Receive the response from the destination server
+        response = destination_socket.recv(4096)
+        print(f"Received response: {response.decode()}")
+
+        # Modify the response if needed
+        # ...
+
+        # Forward the modified response to the client
+        client_socket.sendall(response)
+
+        # Close the sockets
+        client_socket.close()
+        destination_socket.close()
+
+    def start(self):
+        while True:
+            client_socket, client_address = self.server_socket.accept()
+            print(f"Accepted connection from {client_address[0]}:{client_address[1]}")
+            client_thread = threading.Thread(target=self.handle_client, args=(client_socket,))
+            client_thread.start()
+
+if __name__ == "__main__":
+    proxy_server = ProxyServer("proxy_server_host", proxy_server_port)
+    proxy_server.start()
