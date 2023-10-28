333724d8ab91106c4c0aeed914080e34ad4a43b8
vIPERexe2 <148722243+vIPERexe2@users.noreply.github.com>
2023-10-28 23:58:05 +0300

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -1,48 +1,25 @@
-import socket
-import threading
+import requests
+import stem.process
 
-class ProxyServer:
-    def __init__(self, host, port):
-        self.host = host
-        self.port = port
-        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server_socket.bind((self.host, self.port))
-        self.server_socket.listen(5)
-        print(f"Proxy server started on {self.host}:{self.port}")
+# Define a list of Tor SOCKS proxy ports and IP addresses
+tor_proxies = [
+    ('103.176.108.1', 1338),
+    ('85.112.193.37', 8080),
+    ('58.20.82.103', 2323),
+    ('190.113.40.40', 999),
+    ("80.249.112.162", 80),
+    ('202.40.177.69', 80)
+]
 
-    def handle_client(self, client_socket):
-        request = client_socket.recv(4096)
-        print(f"Received request: {request.decode()}")
+# Iterate through the list of Tor proxies and make requests
+for ip, port in tor_proxies:
+    with stem.process.launch_tor_with_config(
+        config={
+            'SocksPort': str(port),  # Use the specified Tor SOCKS port
+        },
+    ) as tor_process:
+        session = requests.Session()
+        session.proxies = {'http': f'socks5://{ip}:{port}', 'https': f'socks5://{ip}:{port}'}
 
-        # Modify the request if needed
-        # ...
-
-        # Forward the modified request to the destination server
-        destination_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        destination_socket.connect(("destination_server_host", destination_server_port))
-        destination_socket.sendall(request)
-
-        # Receive the response from the destination server
-        response = destination_socket.recv(4096)
-        print(f"Received response: {response.decode()}")
-
-        # Modify the response if needed
-        # ...
-
-        # Forward the modified response to the client
-        client_socket.sendall(response)
-
-        # Close the sockets
-        client_socket.close()
-        destination_socket.close()
-
-    def start(self):
-        while True:
-            client_socket, client_address = self.server_socket.accept()
-            print(f"Accepted connection from {client_address[0]}:{client_address[1]}")
-            client_thread = threading.Thread(target=self.handle_client, args=(client_socket,))
-            client_thread.start()
-
-if __name__ == "__main__":
-    proxy_server = ProxyServer("proxy_server_host", proxy_server_port)
-    proxy_server.start()
+        response = session.get("https://example.com")
+        print(f"Proxy: {ip}:{port}, Status Code: {response.status_code}")
