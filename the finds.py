81c6869bed5557a5b5fe15965419f4927907229c
vIPERexe2 <148722243+vIPERexe2@users.noreply.github.com>
2023-10-30 01:24:28 +0200

    Update the finds.py

--- the finds.py
+++ the finds.py
@@ -1,17 +1,15 @@
-import re
+def find_gmails(name, last_name, num_gmails):
+    gmails = []
+    for i in range(1, num_gmails+1):
+        gmail = f"{name.lower()}.{last_name.lower()}{i}@gmail.com"
+        gmails.append(gmail)
+    return gmails
 
-def find_gmails(name, lastname):
-    email_regex = re.compile(r'[a-zA-Z0-9._%+-]+@gmail.com')
-    test_emails = ['test1@gmail.com', 'test2@gmail.com', 'test3@gmail.com']
-    full_name = name.lower() + '.' + lastname.lower()
-    possible_emails = [full_name + '@gmail.com', full_name + '1@gmail.com', full_name + '2@gmail.com']
-    valid_emails = []
-    for email in possible_emails:
-        if email_regex.match(email):
-            valid_emails.append(email)
-    print('Valid Gmails:')
-    for email in valid_emails:
-        print(email)
-    print('Test Gmails:')
-    for email in test_emails:
-        print(email)
+name = input("Enter the name: ")
+last_name = input("Enter the last name: ")
+num_gmails = int(input("Enter the number of Gmail addresses to generate: "))
+
+found_gmails = find_gmails(name, last_name, num_gmails)
+print("Gmail addresses found:")
+for gmail in found_gmails:
+    print(gmail)
