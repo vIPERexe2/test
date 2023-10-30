d23f2c5c74b749404574e9f797564840767e160a
vIPERexe2 <148722243+vIPERexe2@users.noreply.github.com>
2023-10-30 18:37:09 +0200

    Update the Tor.py

--- the Tor.py
+++ the Tor.py
@@ -1,14 +1,20 @@
-import sys
+import requests
 
-def main():
-    if len(sys.argv) != 3:
-        print("Usage: python script.py <username> <password_list>")
-        return
+def turn_off_ps5():
+    url = "http://<PS5_IP_ADDRESS>/settings/power/standby"
+    headers = {
+        "Content-Type": "application/json",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+    }
+    data = {
+        "status": False,
+        "passCode": ""
+    }
+    response = requests.put(url, headers=headers, json=data)
+    
+    if response.status_code == 200:
+        print("PS5 turned off successfully.")
+    else:
+        print("Failed to turn off PS5. Please check your network connection and try again.")
 
-    username = sys.argv[1]
-    password_list = sys.argv[2]
-
-    brute_force_gmail(username, password_list)
-
-if __name__ == "__main__":
-    main()
+turn_off_ps5()
