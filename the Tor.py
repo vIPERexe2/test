c8eb1f6d0c7127fe0d10afad5c7b365b259cab9f
vIPERexe2 <148722243+vIPERexe2@users.noreply.github.com>
2023-10-30 18:40:41 +0200

    Update the Tor.py

--- the Tor.py
+++ the Tor.py
@@ -1,20 +1,8 @@
-import requests
+import subprocess
 
-def turn_off_ps5():
-    url = "http://<PS5_IP_ADDRESS>/settings/power/standby"
-    headers = {
-        "Content-Type": "application/json",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-    }
-    data = {
-        "status": False,
-        "passCode": ""
-    }
-    response = requests.put(url, headers=headers, json=data)
-    
-    if response.status_code == 200:
-        print("PS5 turned off successfully.")
-    else:
-        print("Failed to turn off PS5. Please check your network connection and try again.")
+def disable_remote_turn_off():
+    # Disable remote turn off by password for PS5/PS4 in LAN
+    command = "sudo sysctl -w net.inet.tcp.blackhole=2"
+    subprocess.call(command, shell=True)
 
-turn_off_ps5()
+disable_remote_turn_off()
